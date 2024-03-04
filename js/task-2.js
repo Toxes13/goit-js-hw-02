8447fa06d3a7ef90e2c4c9ec045d860425ad6788
Toxes13 <a.shishov1987@icloud.com>
2024-03-04 22:17:42 +0200

    add hometask

--- js/task-2.js
+++ js/task-2.js
@@ -1,15 +1,17 @@
-function getShippingMessage(country, price, deliveryFee) {
-    const totalPrice = price + deliveryFee;
-    const message = `"Shipping to ${country} will cost ${totalPrice} credits"`;
-    // console.log(message);
-    return message;
-};
 
-const order = getShippingMessage("Australia", 120, 50);
-const order2 = getShippingMessage("Germany", 80, 20);
-const order3 = getShippingMessage("Sweden", 100, 20);
+function formatMessage(message, maxLength) {
 
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+    if (message.length <= maxLength) {
+        return message.slice();
+    }
+    else {
+        return message.slice(0, maxLength) + '...';
+    }
+}
 
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,22 @@
-function getElementWidth(content, padding, border) {
-    const numContent = Number.parseFloat(content);
-    const numPadding = Number.parseFloat(padding);
-    const numBorder = Number.parseFloat(border);
-    const result = numContent + numPadding * 2 + numBorder * 2;
-    return result;
+function checkForSpam(message) {
+
+    const messageReg = message.toLowerCase();
+    const hasSpam = messageReg.includes("spam") || messageReg.includes("sale");
+
+    if (hasSpam) {
+        return `true`;
+    }
+    else {
+        return `false`;
+    }
 }
 
-const task = getElementWidth("50px", "8px", "4px");
-const task2 = getElementWidth("60px", "12px", "8.5px");
-const task3 = getElementWidth("200px", "0px", "0px");
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
